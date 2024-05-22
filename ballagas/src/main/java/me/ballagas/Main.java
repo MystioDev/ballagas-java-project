f52f2665a78bf66b695b2a19bb91f22c8084e42f
Varadi Marcell <varadi_marcell@mathiasz.local>
2024-05-22 13:17:25 +0200

    init db

--- ballagas/src/main/java/me/ballagas/Main.java
+++ ballagas/src/main/java/me/ballagas/Main.java
@@ -1,7 +1,16 @@
 package me.ballagas;
 
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            Database db = new Database();
+
+            db.InitDatabase();
+            db.InitTable();
+        } catch (SQLException err) {
+            System.out.println(err.getMessage());
+        }
     }
 }
